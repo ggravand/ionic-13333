e856f226e4876f429f6a056e3990c0eb67e12d7b
GGravand <ggravand1998@gmail.com>
2017-11-04 13:16:50 +0000

    no reordering beyond visible area

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  items: number[];
+  
   constructor(public navCtrl: NavController) {
-
+    this.items = new Array(50);
+    for (var i=0; i<this.items.length; i++) {
+      this.items[i] = i;
+    }
+  }
+  
+  reorderItems(indeces) {
+    console.log('reorder: ' + JSON.stringify(indeces));
+    let item = this.items[indeces.from];
+    this.items.splice(indeces.from, 1);
+    this.items.splice(indeces.to, 0, item);
   }
 
 }
